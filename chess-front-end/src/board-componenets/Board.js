133a5534343382c963d95137e5b04b8323f49632
Jonathan Nakagawa <jonathannakagawa@Lynettes-Mac-mini.local>
2019-05-16 22:19:54 -1000

    Altered board to be 8x8

--- chess-front-end/src/board-componenets/Board.js
+++ chess-front-end/src/board-componenets/Board.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Tile} from './Tile.js';
+import '../CSS/Board.css';
 
 export class Board extends React.Component {
     renderTile(){
@@ -14,13 +15,68 @@ export class Board extends React.Component {
                 {this.renderTile()}
                 {this.renderTile()}
                 {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
               </div>
               <div className="board-row">
                 {this.renderTile()}
                 {this.renderTile()}
                 {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
               </div>
               <div className="board-row">
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+              </div>
+              <div className="board-row">
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+              </div>
+              <div className="board-row">
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+              </div>
+              <div className="board-row">
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+              </div>
+              <div className="board-row">
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
+                {this.renderTile()}
                 {this.renderTile()}
                 {this.renderTile()}
                 {this.renderTile()}
